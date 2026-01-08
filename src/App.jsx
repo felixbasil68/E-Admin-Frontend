7764901f1556b545d3d50e3fc2554b7cbedec1cc
felixbasil68 <felixbasil12@gmail.com>
2026-01-08 23:51:46 +0530

    Updated feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Dashboard from './pages/Dashboard';
 import Products from './pages/Products';
